83efcab8e08cbfd1dc147202e8a8d9d7ada3aa6f
Kxrr <hi@kxrr.us>
2017-05-11 20:58:17 +0800

    Using pbr to pack.

--- nutstore_cli/__init__.py
+++ nutstore_cli/__init__.py
@@ -1,4 +1,4 @@
 # coding: utf-8
 
 __author__ = 'Kxrr <hi@kxrr.us>'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
--- setup.py
+++ setup.py
@@ -4,18 +4,6 @@ import re
 import setuptools
 import codecs
 
-classes = """
-    Development Status :: 3 - Alpha
-    Environment :: Console
-    Intended Audience :: Developers
-    License :: OSI Approved :: MIT License
-    Topic :: Terminals
-    Programming Language :: Python
-    Programming Language :: Python :: 2.7
-    Programming Language :: Python :: 3
-    Programming Language :: Python :: 3.5
-    Programming Language :: Python :: 3.6
-"""
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'nutstore_cli', '__init__.py')) as f:
@@ -33,22 +21,8 @@ def get_requirements():
 
 
 setuptools.setup(
-    name='nutstore-cli',
-    version=read('version'),
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
-    url='https://github.com/Kxrr/nutstore-cli',
-    license='MIT',
-    author='Kxrr',
-    author_email='hi@kxrr.us',
-    description='A command-line interface for NutStore based on WebDAV.',
-    classifiers=[s.strip() for s in classes.split('\n') if s],
-    install_requires=get_requirements(),
-    include_package_data=True,
-    dependency_links=['git+https://github.com/Kxrr/easywebdav.git#egg=easywebdav-1.2.2', ],
-    entry_points={
-        'console_scripts': [
-            'nutstore-cli = nutstore_cli.__main__:main',
-        ],
 
-    },
+    setup_requires=['pbr'],
+    pbr=True,
 )
